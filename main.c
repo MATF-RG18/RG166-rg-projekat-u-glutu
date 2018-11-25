47c52abdfc3d633d76818b27608e7237df1b4bc1
IvanMihajlovic <ivanmiha.heroj@gmail.com>
2018-11-25 21:11:48 +0100

    igrac skace, prepreka ide ka igracu

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 
+GLfloat jumpMax = 2.3f;
+GLfloat jumpSpeed = 0.05f;
+GLfloat posY = 4.0f;
+GLfloat posZ = -6.0f;
+GLfloat posMax = 3.0f;
+GLfloat posMin = 4.0f;
+GLfloat cone_posZ = 4.0f;
+GLint jumpCount = 0;
 
 static int window_width, window_height;
 
@@ -8,6 +16,8 @@ static int window_width, window_height;
 static void on_keyboard(unsigned char key, int x, int y);
 static void on_reshape(int width, int height);
 static void on_display(void);
+static void jump(int);
+static void cone_move(int);
 
 int main(int argc, char **argv)
 {
@@ -29,7 +39,8 @@ int main(int argc, char **argv)
     glClearColor(0.75, 0.75, 0.75, 0);
     glEnable(GL_DEPTH_TEST);
     glLineWidth(2);
-
+    
+    glutTimerFunc(25, cone_move, 0);
     /* Program ulazi u glavnu petlju. */
     glutMainLoop();
 
@@ -43,6 +54,15 @@ static void on_keyboard(unsigned char key, int x, int y)
         /* Zavrsava se program. */
         exit(0);
         break;
+    case 'w':
+	glutTimerFunc(1,jump, jumpCount);
+	/*if(posY >= posMax)
+		posY = posY - 0.1;
+	else
+		posY = posY + 2.0;*/
+	
+	//glutPostRedisplay();
+	break;
     }
 }
 
@@ -53,6 +73,26 @@ static void on_reshape(int width, int height)
     window_height = height;
 }
 
+static void jump(int value){
+	if(value == 1){
+		jumpCount = 0;
+	}
+	else{
+		posY -= 0.02f;
+		if(posY <= posMax)
+		posY = posMin;
+
+		glutPostRedisplay();
+		glutTimerFunc(25, jump, 0);
+		jumpCount = 1;
+	}
+}
+
+static void cone_move(int value){
+	cone_posZ -= 0.1;
+	glutPostRedisplay();
+	glutTimerFunc(25,cone_move, 0);
+}
 static void on_display(void)
 {
     
@@ -73,15 +113,25 @@ static void on_display(void)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(
-            10/5, 20/5, 30/5,
+            15, 1, 0,
             0, 0, 0,
-            0, 1, 0
+            1, 0, 0
         );
-
-    glColor3f(0, 0, 1);
+    //igrac
+    glColor3f(0, 0.3,0.7);
+    glPushMatrix();
+    glTranslatef(0,posY, posZ);
     glutSolidCube(1);
+    glPopMatrix();
     
-    glPopMatrix();//moda ni vo ne treba
-    /* Nova slika se salje na ekran. */
+    //prepreka
+    glColor3f(0,0,0);
+    glPushMatrix();
+    glTranslatef(0,0,cone_posZ);
+    glRotatef(90,1,0,0);
+    glutSolidCone(0.3,1,10,1);
+    glPopMatrix();
+
+    glutPostRedisplay();
     glutSwapBuffers();
 }
