872345be3fb9d512a049f1311009b42fe52a950e
IvanMihajlovic <ivanmiha.heroj@gmail.com>
2018-12-02 23:47:01 +0100

    Popravljen bag sa skakanjem

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 
 GLfloat jumpMax = 2.3f;
 GLfloat jumpSpeed = 0.05f;
-GLfloat posY = 4.0f;
-GLfloat posZ = -6.0f;
-GLfloat posMax = 3.0f;
-GLfloat posMin = 4.0f;
-GLfloat cone_posZ = 4.0f;
-GLint jumpCount = 0;
+GLfloat player_posY = -4.0f;
+GLfloat player_posX = -6.0f;
+GLfloat posMax = -2.0f;
+GLfloat posMin = -4.0f;
+GLfloat cone_posX = 4.0f;
+GLint jump_count = 1;
 
 static int window_width, window_height;
 
@@ -36,7 +36,7 @@ int main(int argc, char **argv)
     glutDisplayFunc(on_display);
 
     /* Obavlja se OpenGL inicijalizacija. */
-    glClearColor(0.75, 0.75, 0.75, 0);
+    glClearColor(0, 0, 0, 1);
     glEnable(GL_DEPTH_TEST);
     glLineWidth(2);
     
@@ -55,13 +55,8 @@ static void on_keyboard(unsigned char key, int x, int y)
         exit(0);
         break;
     case 'w':
-	glutTimerFunc(1,jump, jumpCount);
-	/*if(posY >= posMax)
-		posY = posY - 0.1;
-	else
-		posY = posY + 2.0;*/
-	
-	//glutPostRedisplay();
+	if(jump_count == 1)
+		glutTimerFunc(0,jump, 0);
 	break;
     }
 }
@@ -74,34 +69,40 @@ static void on_reshape(int width, int height)
 }
 
 static void jump(int value){
-	if(value == 1){
-		jumpCount = 0;
+	
+	if(player_posY <= posMax && jump_count == 1){
+		player_posY += 0.1f;
 	}
-	else{
-		posY -= 0.02f;
-		if(posY <= posMax)
-		posY = posMin;
+	else if(player_posY >= posMax){
+		jump_count = 0;
+	}
+	
+	if(player_posY >= posMin && jump_count == 0)
+		player_posY -= 0.1f ;
+	else if(player_posY <= posMin){
+		player_posY = posMin;
+		jump_count = 1;
+		return;
+	}
 
-		glutPostRedisplay();
-		glutTimerFunc(25, jump, 0);
-		jumpCount = 1;
-	}
+	glutPostRedisplay();
+	glutTimerFunc(1000/60, jump, 0);
 }
 
 static void cone_move(int value){
-	cone_posZ -= 0.1;
-	glutPostRedisplay();
-	glutTimerFunc(25,cone_move, 0);
+	if(cone_posX >= -4.0){
+		cone_posX -= 0.1;
+		glutPostRedisplay();
+		glutTimerFunc(25,cone_move, 0);
+	}
 }
 static void on_display(void)
 {
     
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    /* Podesava se viewport. */
     glViewport(0, 0, window_width, window_height);
 
-    /* Podesava se projekcija. */
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(
@@ -109,26 +110,40 @@ static void on_display(void)
             window_width/(float)window_height,
             1, 25);
 
-    /* Podesava se tacka pogleda. */
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(
-            15, 1, 0,
+            0, 0, 15,
             0, 0, 0,
-            1, 0, 0
+            0, 1, 0 
         );
+	
+    glBegin(GL_LINES);
+        glColor3f(1,0,0);
+        glVertex3f(0,0,0);
+        glVertex3f(10,0,0);
+        
+        glColor3f(0,1,0);
+        glVertex3f(0,0,0);
+        glVertex3f(0,10,0);
+        
+        glColor3f(0,0,1);
+        glVertex3f(0,0,0);
+        glVertex3f(0,0,10);
+    glEnd();
+
     //igrac
     glColor3f(0, 0.3,0.7);
     glPushMatrix();
-    glTranslatef(0,posY, posZ);
+    glTranslatef(player_posX, player_posY, 0);
     glutSolidCube(1);
     glPopMatrix();
     
     //prepreka
-    glColor3f(0,0,0);
+    glColor3f(0,1,0);
     glPushMatrix();
-    glTranslatef(0,0,cone_posZ);
-    glRotatef(90,1,0,0);
+    glTranslatef(cone_posX,0,0);
+    glRotatef(-90,1,0,0);
     glutSolidCone(0.3,1,10,1);
     glPopMatrix();
 
